b227ecc4eb11a8795a9485647cfb28b4e0bac50e
nik3jn <78638888+nik3jn@users.noreply.github.com>
2024-10-22 19:52:30 +1000

    задание 3.3 модуля 3 на проверку

---
+++ module_3_3.py
@@ -0,0 +1,11 @@
+def print_params(a = 1, b= 'строка', c = True):
+    print(a, b, c)
+print_params()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+values_list = [2.5, 'Текст', False]
+values_dict = {'a': 3, 'b': 'Слово', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
